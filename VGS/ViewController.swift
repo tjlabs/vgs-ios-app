7f0e14d10abe66192bc8b4bdb62b131f74594cab
Leo <dev@tjlabscorp.com>
2025-05-07 10:52:03 +0900

    Initial Commit

---
+++ VGS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VGS
+//
+//  Created by 신동현 on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VGSUITests/VGSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  VGSUITestsLaunchTests.swift
+//  VGSUITests
+//
+//  Created by 신동현 on 5/7/25.
+//
+
+import XCTest
+
+final class VGSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
