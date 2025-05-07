c0593097d34831d7fb91b9de0bda0dad211b5388
Leo <dev@tjlabscorp.com>
2025-05-07 14:22:15 +0900

    feat: UserManager
    manager for user information

--- VGS/ViewModel/UserManager.swift
+++ VGS/ViewModel/UserManager.swift
@@ -1,7 +1,31 @@
-//
-//  ProfileManager.swift
-//  VGS
-//
-//  Created by 신동현 on 5/7/25.
-//
 
+import Foundation
+import UIKit
+
+class UserManager {
+    static let shared = UserManager()
+    
+    var isLoadFromCache: Bool = false
+    var userProfile = UserProfile(carNumber: "")
+    
+    private init() { }
+
+    func loadProfileFromCache() {
+        let defaults = UserDefaults.standard
+        if let carNumber = defaults.string(forKey: "VgsUserCarNumber") {
+            userProfile.carNumber = carNumber
+            isLoadFromCache = true
+        } else {
+            isLoadFromCache = false
+        }
+        print("UserManager (Load From Cahce) : userProfile = \(userProfile)")
+    }
+    
+    func saveProfileToCache() {
+        let defaults = UserDefaults.standard
+        defaults.set(userProfile.carNumber, forKey: "VgsUserCarNumber")
+        
+        print("UserManager (Save To Cahce) : userProfile = \(userProfile)")
+        defaults.synchronize()
+    }
+}
