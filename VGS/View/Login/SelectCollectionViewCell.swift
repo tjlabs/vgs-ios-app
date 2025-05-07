8503659a3351ddd53ef0b1bb89c167dcde76aff0
Leo <dev@tjlabscorp.com>
2025-05-07 15:22:16 +0900

    feat: SelectCollectionViewCell
    cell for car selection

--- VGS/View/Login/SelectCollectionViewCell.swift
+++ VGS/View/Login/SelectCollectionViewCell.swift
@@ -1,12 +1,43 @@
-//
-//  SelectCollectionViewCell.swift
-//  VGS
-//
-//  Created by 신동현 on 5/7/25.
-//
 
 import UIKit
+import SnapKit
+import RxCocoa
+import RxSwift
+import RxRelay
+import Then
 
 class SelectCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "SelectCollectionViewCell"
     
+    private let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hex: "#322E2E")
+        view.cornerRadius = 15
+        view.addShadow(location: .rightBottom, color: .black, opacity: 0.2)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    private func setupLayout() {
+        addSubview(cellView)
+        cellView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    func configure(data: SelectCellItem) {
+    }
 }
