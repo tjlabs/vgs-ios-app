5c5c73829cccd8934dc0d03a1b0d0a4f15209eeb
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2025-06-24 14:32:55 +0900

    feat: tmap test
    test

--- VGS/View/MainViewController.swift
+++ VGS/View/MainViewController.swift
@@ -6,6 +6,10 @@ import RxSwift
 import RxRelay
 import Then
 import TJLabsAuth
+import Combine
+import TmapUISDK
+import TmapNaviSDK
+import VSMSDK
 
 class MainViewController: UIViewController, BottomNavigationViewDelegate, NaviArrivalDelegate {
     
@@ -74,6 +78,8 @@ class MainViewController: UIViewController, BottomNavigationViewDelegate, NaviAr
         bottomNavigationView.delegate = self
         kakaoNaviView.delegate = self
         
+        initSDK()
+        
         startTimer()
         
         NotificationCenter.default.addObserver(self,
@@ -98,7 +104,15 @@ class MainViewController: UIViewController, BottomNavigationViewDelegate, NaviAr
     var positionTimer: DispatchSourceTimer?
     let TIMER_INTERVAL: TimeInterval = 5.0
     
+    // TMAP
+    let tmapNaviView = TMapNaviView()
+    private var sdkInitComplete: Bool = false
     private var locationAlert: UIAlertController? = nil
+    var authCancelable: Set<AnyCancellable> = []
+    
+    var apiKey: String = "wgk9bctFRs3tsVE91HxRz7ZieprP1beu2TgKttbs"
+    var userKey: String = ""
+    var deviceKey: String = ""
     
     private func setBottomNavigationHeight() {
         let totalHeight = view.frame.height
@@ -117,8 +131,14 @@ class MainViewController: UIViewController, BottomNavigationViewDelegate, NaviAr
             make.bottom.equalToSuperview().inset(self.bottomNavigationHeight-20)
         }
         
-        view.addSubview(kakaoNaviView)
-        kakaoNaviView.snp.makeConstraints { make in
+//        view.addSubview(kakaoNaviView)
+//        kakaoNaviView.snp.makeConstraints { make in
+//            make.top.leading.trailing.equalToSuperview()
+//            make.bottom.equalToSuperview().inset(self.bottomNavigationHeight-20)
+//        }
+        
+        view.addSubview(tmapNaviView)
+        tmapNaviView.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
             make.bottom.equalToSuperview().inset(self.bottomNavigationHeight-20)
         }
@@ -275,4 +295,232 @@ class MainViewController: UIViewController, BottomNavigationViewDelegate, NaviAr
             self.present(alter, animated: true, completion: nil)
         }
     }
+    
+    private func continueAlert(str : String?){
+        if(str?.isEmpty != nil){
+            let alert = UIAlertController(title: "이어가기 안내",
+                                          message: "\(String(describing: str!))(으)로 경로안내를 이어서 받으시겠습니까?\n '아니오'를 선택하시면 이어가기 정보가 삭제됩니다.",
+                                          preferredStyle: UIAlertController.Style.alert)
+            
+            let logOkAction = UIAlertAction(title: "네", style: UIAlertAction.Style.default){
+                (action: UIAlertAction) in
+                self.continueDrive()
+            }
+            let logNoAction = UIAlertAction(title: "아니오", style: UIAlertAction.Style.destructive){
+                (action: UIAlertAction) in
+                TmapUISDKManager.shared.clearContinueDriveInfo()
+            }
+            alert.addAction(logNoAction)
+            alert.addAction(logOkAction)
+            present(alert, animated: true, completion: {})
+        }
+    }
+    
+    private func setTmapUserInfo() {
+        if let apiKey = Bundle.main.object(forInfoDictionaryKey: "TMAP_API_KEY") as? String {
+            self.apiKey = apiKey
+            self.deviceKey = UIDevice.modelName
+            self.userKey = "TJLabs_\(getCurrentTimeInMilliseconds())"
+        }
+    }
+    
+    private func initSDK(){
+        setTmapUserInfo()
+        authCancelable.cancelAll()
+        
+        TmapUISDKManager.shared.stateSubject
+            .receive(on: DispatchQueue.main, options: nil)
+            .sink(receiveValue: { [weak self] (state) in
+            switch state {
+                case .completed:
+                    // SDK의 초기화가 완료되었음을 알림
+                    self?.sdkInitComplete = true
+                    UIView.showMessage("SDK초기화가 완료되었습니다.")
+                    self?.setConfig()
+                    self?.embedTMapNavi()
+                case let .requestPermission(permission):
+                    // Tmap의 화면이 노출되지 않은 상태에서 권한 요청이 필요한 경우 알림
+                    if (permission.locationRequest.isEmpty) {
+                        self?.hideLocationAlert()
+                    } else {
+                        self?.showLocationAlert()
+                    }
+                case .dismissReq:
+                    // Tmap에서 요청했던 화면이 종료되었을때 알림
+                    self?.dismiss(animated: false, completion: nil)
+                case let .dismissNRequestPermission(permission) :
+                    // Tmap에서 요청했던 화면이 종료된 후 특정 권한이 필요한 경우 알림
+                    // 사용자에게 노출했던 화면을 닫고 권한 popup을 출력한다.
+                    self?.dismiss(animated: false, completion: {
+                        if (permission.locationRequest.isEmpty) {
+                            self?.hideLocationAlert()
+                        } else {
+                            self?.showLocationAlert()
+                        }
+                    })
+                case let .savedDriveInfo(str?):
+                    self?.continueAlert(str: str)
+                default:
+                    print("init state: \(state)")
+            }
+        }).store(in: &authCancelable)
+        
+        //initOption
+        var initOption = UISDKInitOption()
+        initOption.clientDeviceId = UUID().uuidString
+        
+        initOption.clientApiKey = apiKey
+        initOption.userKey = userKey
+        initOption.deviceKey = deviceKey
+
+        initOption.uiWindow = UIApplication.shared.windows.first
+        TmapUISDKManager.shared.initSDK(initOption: initOption)
+    }
+    
+    func setConfig() {
+        var sdkConfig = UISDKConfigOption()
+        sdkConfig.carType = .normal                         // 자동차의 종류를 설정합니다.
+        sdkConfig.fuelType = .gas                           // 자동차의 유종을 설정합니다.
+        sdkConfig.showTrafficAccident = true                // map상에서 유고정보(사고정보) 및 교통량의 표시여부를 설정합니다.
+        sdkConfig.mapTextSize = .large                      // map의 text의 크기를 설정합니다.
+        sdkConfig.nightMode = .auto                         // dark mode의 동작여부를 설정합니다.
+        sdkConfig.isUseSpeedReactMapScale = true            // 속도반응형 지도의 사용여부를 설정합니다.
+        sdkConfig.isShowTrafficInRoute = true               // 안내되는 경로상에 교통량의 표시여부를 설정합니다.
+        sdkConfig.showExitPopupWhenStopDriving = true       // 주행 종료시 popup의 출력 여부
+        sdkConfig.useRealTimeAutoReroute = true             // 주행 시 실시간 경로탐색의 실행 여부
+        
+        sdkConfig.mapViewDelegate = self
+        
+        TmapUISDKManager.shared.setConfig(config: sdkConfig)
+    }
+    
+    // Route
+    private func continueDrive(){
+        let sdkAvail = self.sdkInitComplete
+        if sdkAvail, let sdkVC = TmapUISDKManager.shared.getViewController() {
+            // 3. sdkVC를 child로 추가 (iOS fragment-like pattern)
+            self.addChild(sdkVC)
+            tmapNaviView.addSubview(sdkVC.view)
+            sdkVC.view.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                sdkVC.view.topAnchor.constraint(equalTo: tmapNaviView.topAnchor),
+                sdkVC.view.bottomAnchor.constraint(equalTo: tmapNaviView.bottomAnchor),
+                sdkVC.view.leadingAnchor.constraint(equalTo: tmapNaviView.leadingAnchor),
+                sdkVC.view.trailingAnchor.constraint(equalTo: tmapNaviView.trailingAnchor)
+            ])
+            sdkVC.didMove(toParent: self)
+            
+            TmapUISDKManager.shared.continueDrive()
+        } else {
+            print("vc is nil")
+            UIView.showMessage("에러가 발생 했습니다.\n 다시 시도해주세요. - VC NIL")
+        }
+    }
+    
+    func embedTMapNavi() {
+        let sdkAvail = self.sdkInitComplete
+        if sdkAvail, let sdkVC = TmapUISDKManager.shared.getViewController() {
+            // 3. sdkVC를 child로 추가 (iOS fragment-like pattern)
+            self.addChild(sdkVC)
+            tmapNaviView.addSubview(sdkVC.view)
+            sdkVC.view.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                sdkVC.view.topAnchor.constraint(equalTo: tmapNaviView.topAnchor),
+                sdkVC.view.bottomAnchor.constraint(equalTo: tmapNaviView.bottomAnchor),
+                sdkVC.view.leadingAnchor.constraint(equalTo: tmapNaviView.leadingAnchor),
+                sdkVC.view.trailingAnchor.constraint(equalTo: tmapNaviView.trailingAnchor)
+            ])
+            sdkVC.didMove(toParent: self)
+            
+            // 4. 경로 요청 (SDK에 따라 이 시점에서 호출 가능)
+            let latitude_goal = 37.16270985567856
+            let longitude_goal = 127.32467624370436
+            let name_goal = "GATE#6"
+            
+            let destination = Point(longitude: longitude_goal, latitude: latitude_goal, name: name_goal)
+            TmapUISDKManager.shared.requestRoute(destination: destination)
+        } else {
+            UIView.showMessage("에러가 발생 했습니다.\n 다시 시도해주세요. - VC NIL")
+        }
+    }
+}
+
+extension MainViewController: UISDKMapViewDelegate {
+    func addMarker(imageName: String, markerId: String = "", text: String? = nil, point: VSMMapPoint) -> VSMMarkerPoint? {
+        let params = VSMMarkerPointParams()
+        params.icon = MarkerImage(imageName: imageName, bundle: Bundle.main)
+        params.iconSize = params.icon.image?.size ?? .zero
+        params.iconAnchor = CGPoint(x: 0.5, y: 1)
+        params.text = text ?? ""
+        params.position = point
+        params.showPriority = 9999
+        return VSMMarkerPoint(id: markerId, params: params)
+    }
+    
+    ///polyline
+    func addPolyline(fromWgs: Coordinate, toWgs: Coordinate, imageName: String, markerId: String, renderOrder: MarkerRenderOrder = .RENDERING_ORDER_BEFORE_POINT_MARKER) -> VSMMarkerPolyline?{
+        //guard let image = UIImage(named: imageName, in: Bundle.main, compatibleWith: nil) else { return nil}
+        let params = VSMMarkerPolylineParams()
+       
+        //params.icon = MarkerImage(image: image)
+        params.points = [VSMMapPoint(longitude: fromWgs.longitude, latitude: fromWgs.latitude), VSMMapPoint(longitude: toWgs.longitude, latitude: toWgs.latitude)]
+        params.lineWidth = 3
+        params.showPriority = 9999
+        params.renderOrder = UInt32(renderOrder.rawValue)
+        params.touchable = false
+        return VSMMarkerPolyline(id: markerId, params: params)
+   }
+    
+    func viewDidLoad(_ mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) {
+        print("ViewController - mapView - viewDidLoad \(type)")
+        // do something
+        
+        //SKT 타워
+        let point = VSMMapPoint(longitude: 126.985173, latitude: 37.566411)
+      
+        // marker의 표현
+        let marker = self.addMarker(imageName: "icon01", text: "SKT 타워", point: point)
+        mapView.getMarkerManager().addMarker(marker)
+
+        // 선 표현
+        let dash = self.addPolyline(fromWgs: Coordinate(latitude: point.latitude, longitude: point.longitude),
+                                    toWgs: Coordinate(latitude: point.latitude + 0.001, longitude: point.longitude + 0.001),
+                                            imageName: "",
+                                            markerId: "map_goal_dash_marker_id1",
+                                            renderOrder: .RENDERING_ORDER_BEFORE_POINT_MARKER)
+        mapView.getMarkerManager().addMarker(dash)
+    }
+    
+    func viewWillAppear(_ mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) {
+        // do something
+        print("ViewController - mapView - viewWillAppear \(type)")
+    }
+    
+    func viewDidAppear(_ mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) {
+        // do something
+        print("ViewController - mapView - viewDidAppear \(type)")
+    }
+    
+    func viewWillDisappear(_ mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) {
+        // do something
+        print("ViewController - mapView - viewWillDisappear \(type)")
+    }
+    
+    func viewDidDisappear(_ mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) {
+        // do something
+        print("ViewController - mapView - viewDidDisappear \(type)")
+    }
+    
+    func markerSelected(_ marker: VSMMarkerBase, addedMapView mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) -> Bool {
+        guard let markerId = marker.markerID else { return false }
+        print("ViewController - mapView - markerSelected markerId:\(markerId) \(type)")
+        return true
+    }
+    
+    func calloutPopupSelected(_ marker: VSMMarkerBase, addedMapView mapView: VSMNavigationView, mapViewType type: MapViewFunctionType) -> Bool {
+        guard let markerId = marker.markerID else { return false }
+        print("ViewController - mapView - calloutPopupSelected markerId:\(markerId) \(type)")
+        return true
+    }
+   
 }
